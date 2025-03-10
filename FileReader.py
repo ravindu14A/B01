a76c86f330295120caf38bc32399df17846ac17f
Vektir <viktor@velichkov.net>
2025-03-10 10:42:07 +0100

    fixed correlation, covariance, added PZITRF file fore ease of use

--- FileReader.py
+++ FileReader.py
@@ -30,10 +30,15 @@ def getDayData(day):
 				line=line.split()
 				index_1 = int(line[0])%3 - 1
 				index_2 = int(line[1])%3 - 1
-				covariance_matrices[i,index_1,index_2] = float(line[2])
-				covariance_matrices[i,index_2,index_1] = float(line[2])
+
+				correlation = float(line[2])
+				covariance = correlation * covariance_matrices[i,index_1,index_1] * covariance_matrices[i,index_2,index_2]
+				covariance_matrices[i,index_1,index_2] = covariance
+				covariance_matrices[i,index_2,index_1] = covariance
 
 	return names, coordinates, covariance_matrices
 		# print(names[:5])
 		# print(coordinates[:5])
 		# print(covariance_matrices[:5])
+names, coordinates, covariance_matrices = getDayData(366)
+print(covariance_matrices[:3])
