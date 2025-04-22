bd806994571c8ce3300a0d2bb86cd5c83e1c12d9
LauraKaczmarzyk <laura.kaczmarzyk@wp.pl>
2025-04-22 16:42:04 +0200

    kkk

---
+++ predictions/fitting_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import pickle
+from preprocessing.internal_dataclass.dataset import Station, GeoDataset
+
+#import the data as a dataframe for ease of use
+#define longtitide and latitude as long and lat
+def combine():
+    #loop through data frame and find their distance as a combination of longtitude and latitude
+    for
+        ...
+    dist = np.sqrt(long**2 + lat**2)
+
+
+
+
+
+with open('path_to_file.pkl', 'rb') as f:
+    station_obj = pickle.load(f)
+
