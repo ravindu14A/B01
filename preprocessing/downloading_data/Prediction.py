0fd9269dac95e41f833a8feabc52845feb643068
ravindu14A <bandaranet9@gmail.com>
2025-05-12 08:54:33 +0200

    e

--- preprocessing/downloading_data/Prediction.py
+++ preprocessing/downloading_data/Prediction.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit, bisect
 
-# --- Load data ---
+
 station = "PHUK"
 country = "Thailand"
 filepath = f"../processed_data/{country}/Final/{station}.pkl"
@@ -14,19 +14,18 @@ with open(filepath, 'rb') as f:
     data = pickle.load(f)
     df = pd.DataFrame(data)
 
-# --- Preprocessing ---
+
 df['date'] = pd.to_datetime(df['date'])
 start_date = df["date"].iloc[0]
 end_date = pd.to_datetime("2004-12-15")
 df['days_from_ref'] = (df['date'] - start_date).dt.days
 
-# --- First segment for linear fit ---
 mask = (df['date'] >= start_date) & (df['date'] <= end_date)
 df_subset = df.loc[mask]
 X = df_subset['days_from_ref'].values.reshape(-1, 1)
 y = df_subset['lat'].values
 
-# --- Fit linear regression ---
+
 model = LinearRegression()
 model.fit(X, y)
 slope_per_day = model.coef_[0]
@@ -35,31 +34,31 @@ print(f"Slope (mm/year): {slope_mm_per_year:.3f}")
 
 y_pred = model.predict(X)
 
-# --- Fit exponential recovery model ---
+
 df_fit = df.loc[df['date'] > end_date]
 start_day1 = df_fit["days_from_ref"].iloc[0]
 t_data = df_fit['days_from_ref'].values
 y_data = df_fit['lat'].values
 
-# Model definition
+
 def model_func(t, A, B, c1, c2, d):
     return A * np.exp(-c1 * (t - start_day1)) + B * np.exp(-c2 * (t - start_day1)) + d + slope_per_day * (t - start_day1)
 
-# Fit the model
+
 popt, pcov = curve_fit(model_func, t_data, y_data)
 A, B, c1, c2, d = popt
 param_names = ['A', 'B', 'c1', 'c2', 'd']
 for name, val in zip(param_names, popt):
     print(f"{name} = {val:.6f}")
 
-# Generate future prediction points
+
 years = 340
 safe_end = round(years* 365.25)  # ~547 years
 T = np.arange(start_day1, safe_end, 5)
 y_fit = model_func(T, *popt)
 ref = np.full_like(T, df_fit["lat"].iloc[0])
 
-# Root finding
+
 def f(t):
     return model_func(t, *popt) - df_fit["lat"].iloc[0]
 
@@ -79,7 +78,7 @@ try:
 except Exception as e:
     print("Adjust bisection settings:", e)
 
-# --- Plot ---
+
 T_years = T / 365.25  # ✅ Correct x-axis alignment
 df_years = df['days_from_ref'] / 365.25
 df_subset_years = df_subset['days_from_ref'] / 365.25
